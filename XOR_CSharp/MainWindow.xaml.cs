747b62453ad0d1fa62779ef54ef24ab6320c6d66
KostyaTymko <kostyatymko@gmail.com>
2017-04-10 11:18:08 +0300

    ex7

--- XOR_CSharp/MainWindow.xaml.cs
+++ XOR_CSharp/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace XOR_CSharp
     /// </summary>
     public partial class MainWindow : Window
     {
-        //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        //jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
         public SynchronizationContext uiContext;
         CancellationTokenSource cts;
         static int Key2 = 0;
@@ -34,7 +34,7 @@ namespace XOR_CSharp
 
         public MainWindow()
         {
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            //jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
             uiContext = SynchronizationContext.Current;
             InitializeComponent();
             Button1.IsEnabled = false;
